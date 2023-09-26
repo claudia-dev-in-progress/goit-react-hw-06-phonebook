da6456f2f652ec375b525c0071dadbc0bfe7ab00
claudiapapp <pappileanaclaudia@gmail.com>
2023-09-26 22:05:03 +0300

    add redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,23 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 import { combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
+const persistConfig = {
+  key: "contacts",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
